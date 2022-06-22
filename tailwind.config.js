6c28deee3addad1be723affd4419cd7c36a29727
Nathan Morgan <nathan@fuelingtheweb.com>
2022-06-22 19:02:39 -0400

    Install Laravel DomPdf and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
