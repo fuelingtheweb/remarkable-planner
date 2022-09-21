1bbfe538f77d94d574a8c768e607ce32a5eb0854
Nathan Morgan <nathan@fuelingtheweb.com>
2022-09-21 09:35:38 -0400

    Fit year inside viewport

--- resources/views/year.blade.php
+++ resources/views/year.blade.php
@@ -9,20 +9,22 @@
     @vite(['resources/css/app.css'])
 </head>
 <body>
-    <div class="p-4 mb-8 text-4xl text-center text-white bg-gray-900">
-        {{ $calendar['year'] }}
-    </div>
+    <x-viewport>
+        <div class="p-4 mb-2 text-4xl text-center text-white bg-gray-900">
+            {{ $calendar['year'] }}
+        </div>
 
-    <div class="grid grid-cols-3 gap-8 px-8 pb-8">
-        @foreach ($calendar['months'] as $month)
-            <div>
-                <div class="p-4 mb-4 text-2xl text-center text-white bg-gray-100 dark:bg-gray-800">
-                    {{ $month['month'] }}
+        <div class="grid grid-cols-3 px-2 pb-8 gap-x-2 gap-y-3">
+            @foreach ($calendar['months'] as $month)
+                <div>
+                    <div class="p-1 mb-1 text-xl text-center text-white bg-gray-100 dark:bg-gray-800">
+                        {{ $month['month'] }}
+                    </div>
+
+                    <x-month :weeks="$month['weeks']" />
                 </div>
-
-                <x-month :weeks="$month['weeks']" />
-            </div>
-        @endforeach
-    </div>
+            @endforeach
+        </div>
+    </x-viewport>
 </body>
 </html>
