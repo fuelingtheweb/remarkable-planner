1187b55eaaecbfca789e4b6906eb508c6982138e
Zuzana K <zk0433@gmail.com>
2022-09-27 18:00:24 +0100

    Make year head full width

--- resources/views/components/viewport.blade.php
+++ resources/views/components/viewport.blade.php
@@ -1,12 +1,10 @@
 @if (request()->hasHeader('X-Printing-Pdf'))
     <div class="mx-auto break-before-page">
-        <div class="ml-[52px]">
-            {{ $slot }}
-        </div>
+        {{ $slot }}
     </div>
 @else
     <div class="w-[596px] h-[795px] overflow-hidden border border-black mx-auto my-4 break-before-page">
-        <div class="ml-[52px] h-full border-l border-black">
+        <div class="h-full border-l border-black">
             {{ $slot }}
         </div>
     </div>
